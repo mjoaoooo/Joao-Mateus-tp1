ff147085289ee884342d287b1842a4a3b8203a4f
mjoaoooo <j.mateus@gsuite.iff.edu.br>
2025-10-24 10:38:09 -0300

    Create cli.js

---
+++ cli.js
@@ -0,0 +1,14 @@
+function nome(){
+    console.log("Olá, João Mateus");
+}
+function soma(){
+    let a = parseInt(1);
+    let b = parseInt(1);
+    console.log("a = 1, b = 1\n"+  a + " + " + b + " = " + parseInt(a + b));
+}
+function data(){
+    console.log(new Date().toString());
+}
+nome();
+soma();
+data();
